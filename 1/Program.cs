3b71f9757d64ead9ceabc0643aaeb6efc4e5ea8d
Lera <miss_val@bk.ru>
2022-12-29 12:15:30 +0300

    Решение задачи

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] textArray = new string[] { "df", "dfgtt", "555", "5t", "j,-p", "t68ggg", "ghi888gg" };
+int NewArrayLength = 0;
+    for (int i = 0; i < textArray.GetLength(0); i++)
+    {
+       if (textArray[i].Length >= 3 )
+       {
+        NewArrayLength++;
+       }
+    }
+string [] threeAndLessArray = new string [NewArrayLength];
+int newArrayIndex = 0;
+    for (int i = 0; i < textArray.GetLength(0); i++)
+    {
+       if (textArray[i].Length <= 3 )
+       {
+        threeAndLessArray[newArrayIndex] = textArray[i];
+        newArrayIndex++;
+       }
+    }
+    for (int i = 0; i < threeAndLessArray.GetLength(0); i++)
+    {
+      Console.WriteLine(threeAndLessArray[i]);
+    }
